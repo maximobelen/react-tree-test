e0dcf351419a475ef54db2a2e6b3eb0b0fc44724
maximobelen <maximobelen@me.com>
2017-11-02 21:19:48 -0300

    first commit

---
+++ src/scripts/index.js
@@ -0,0 +1,8 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/Main.js';
+
+const rootEl = document.getElementById('app');
+
+ReactDOM.render(<App/>, rootEl);
