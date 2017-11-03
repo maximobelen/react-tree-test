dccc4224e345243d57813302559f49580fefb0c0
maximobelen <maximobelen@me.com>
2017-11-02 21:25:23 -0300

    fixes

--- src/scripts/index.js
+++
@@ -1,8 +0,0 @@
-import 'core-js/fn/object/assign';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/Main.js';
-
-const rootEl = document.getElementById('app');
-
-ReactDOM.render(<App/>, rootEl);
